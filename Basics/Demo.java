44a12aa59ced0e3964599a3144d8e43e6e296e74
Ansh Rehan <118544177+AnshRehan04@users.noreply.github.com>
2025-01-10 19:44:30 +0530

    Delete Basics directory

--- Basics/Demo.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Demo{
-public static void main(String[] args){
-    System.out.println("hello world");          
-    System.out.print("Enter a number :");
-    Scanner input=new Scanner(System.in);
-    System.out.println("Your number is "+input.nextInt());
-}
-}
